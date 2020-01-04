909d72894604060f69e2f98e3b02b30aabf8286e
Marc Fernandez <mfernandez225@gmail.com>
2020-01-04 09:59:35 -0800

    just got tempeture, humidity, and wind speed to display

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 var apiKey = "a64170d48eba7179e009a291124e0d69";
-
+// The search when button is clicked
 $("#searchCity").on("click", function (event) {
   event.preventDefault();
-
+  // What we need to find
   var cityName = $("#findCity").val();
-  console.log(cityName);
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&apikey=" + apiKey;
 
-  var queryURL = "http://api.openweathermap.org/data/2.5/find?q=" + cityName + "&units=imperial&apikey=" + apiKey;
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    $("#citySearched").text(JSON.stringify(response));
-    console.log(response.list[0].main.temp);
-
+    $("#citySearched").text(response.name);
+    $("#temperature").text("Temperature " + response.main.temp);
+    $("#humidity").text("Humidity " + response.main.humidity);
+    $("#windSpeed").text("Wind Speed " + response.wind.speed);
   })
 })
+
+
+
+
+// $("#uvIndex").text(JSON.stringify(response));
